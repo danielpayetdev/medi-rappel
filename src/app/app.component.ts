16f71d4429b24d8acf038988ce3ebc88414ba5dc
Daniel Payet <danieldu974@gmail.com>
2023-10-29 21:38:08 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { IonApp, IonItem, IonList, IonToggle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonApp, IonList, IonItem, IonToggle],
 })
 export class AppComponent {
-  constructor() {}
+  currentDose = 1;
+
+  onDosePrise() {
+    this.currentDose += 1;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicRouteStrategy } from '@ionic/angular';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideIonicAngular({ mode: 'md' }),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+}).catch((err) => console.log(err));
